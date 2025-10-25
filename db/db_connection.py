5b71718d1f80887c6dfa06b6616a1200a5583903
Thamiris Trentin <thamiristrentinads@gmail.com>
2025-10-25 17:36:00 -0300

    Update db_connection.py

--- db/db_connection.py
+++ db/db_connection.py
@@ -2,10 +2,10 @@ import mysql.connector
 
 def conectar():
     return mysql.connector.connect(
-        host="maglev.proxy.rlwy.net",  # substitui se o host for outro
-        port=25116,  # confirma no painel Railway
-        user="root",  # ou o usuário do teu banco
-        password="tstSBXUgVdJcECuNRvHyOySnItSpyUXA",
-        database="greencode",
+        host="",  # substitui se o host for outro
+        port=,  # confirma no painel Railway
+        user="",  # ou o usuário do teu banco
+        password="",
+        database="",
         charset="utf8mb4"
     )
