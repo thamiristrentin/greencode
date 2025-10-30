1248fbeec3fedb79ddb1e5392df45458af4761c7
Thamiris Trentin <thamiristrentinads@gmail.com>
2025-10-30 11:49:54 -0300

    Atualizar o db_connection.py

--- db/db_connection.py
+++ db/db_connection.py
@@ -2,9 +2,9 @@ import mysql.connector
 
 def conectar():
     return mysql.connector.connect(
-        host="maglev.proxy.rlwy.net",  # substitui se o host for outro
-        port=25116,  # confirma no painel Railway
-        user="root",  # ou o usuário do teu banco
+        host="maglev.proxy.rlwy.net",
+        port=25116,
+        user="root",
         password="tstSBXUgVdJcECuNRvHyOySnItSpyUXA",
         database="greencode",
         charset="utf8mb4"
